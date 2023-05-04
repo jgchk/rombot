072a495566345b67299d69de200f670a517b66a7
Jake Cheek <jake@f-m.fm>
2023-05-03 21:17:03 -0400

    better fetch error logging

--- apps/app/src/lib/fetch.ts
+++ apps/app/src/lib/fetch.ts
@@ -3,6 +3,9 @@ export const fetcher =
   async (...args: Parameters<typeof fetch>): Promise<Response & { json: <T>() => Promise<T> }> => {
     const res = await fetch_(...args)
     if (!res.ok) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const body = await res.json()
+      console.error('Fetch failed', body)
       throw new Error('Fetch failed', { cause: res })
     }
     return res
