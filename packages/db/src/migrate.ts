70f14d0453fe0976fa9423ce849b5369f4107bff
Jake Cheek <jake@f-m.fm>
2023-05-04 16:23:38 -0400

    create migration script

--- packages/db/src/migrate.ts
+++ packages/db/src/migrate.ts
@@ -1,9 +1,21 @@
-import type { BetterSQLite3Database } from 'drizzle-orm/better-sqlite3'
-import { migrate as drizzleMigrate } from 'drizzle-orm/better-sqlite3/migrator'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { migrate as drizzleMigrate } from 'drizzle-orm/node-postgres/migrator'
 import path from 'path'
+import pg from 'pg'
 
 import drizzleConfig from '../drizzle.config.json'
 
 const migrationsFolder = path.resolve(path.join(__dirname, '../', drizzleConfig.out))
 
-export const migrate = (db: BetterSQLite3Database) => drizzleMigrate(db, { migrationsFolder })
+console.log(`Running migrations from ${migrationsFolder}`)
+
+const DATABASE_URL = process.env.DATABASE_URL
+if (!DATABASE_URL) throw new Error('DATABASE_URL is not defined')
+
+console.log(`Connecting to ${DATABASE_URL}`)
+
+const db = drizzle(new pg.Client({ connectionString: DATABASE_URL }), { logger: true })
+
+drizzleMigrate(db, { migrationsFolder })
+  .then(() => 'Ran migrations!')
+  .catch((e) => console.error('Error migrating database', e))
