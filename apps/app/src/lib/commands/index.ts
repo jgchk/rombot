c9e248752705a407e8d98b46141b8cca3eb41550
Jake Cheek <jake@f-m.fm>
2023-05-03 23:54:08 -0400

    add login to commands list

--- apps/app/src/lib/commands/index.ts
+++ apps/app/src/lib/commands/index.ts
@@ -1,4 +1,5 @@
+import { login } from './login'
 import { ping } from './ping'
 
-export const commands = [ping]
+export const commands = [ping, login]
 export const commandMap = new Map(commands.map((command) => [command.data.name, command]))
