b6e2cf02cceb0ab41ce590e58133f53040b5161f
Jake Cheek <jake@f-m.fm>
2023-05-05 03:01:33 -0400

    try zlib fix

--- apps/app/vite.config.js
+++ apps/app/vite.config.js
@@ -1,5 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(`${process.env.PWD}/node_modules/canvas/build/Release:`)
+) {
+  process.env.LD_LIBRARY_PATH = `${process.env.PWD}/node_modules/canvas/build/Release:${
+    process.env.LD_LIBRARY_PATH || ''
+  }`
+}
+
 /** @type {import('vite').UserConfigFn} */
 const defineConfig = ({ mode }) => ({
   plugins: [sveltekit()],
