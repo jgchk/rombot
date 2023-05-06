1f9eddc688127c6a6a47c614a48a509d41a9a5c8
Jake Cheek <jake@f-m.fm>
2023-05-06 14:52:34 -0400

    create node handler for slow commands

--- apps/app/src/routes/api/interactions/node/+server.ts
+++ apps/app/src/routes/api/interactions/node/+server.ts
@@ -1,11 +1,10 @@
 import { error, json } from '@sveltejs/kit'
 import { InteractionResponseType, InteractionType } from 'discord-api-types/v10'
 import type { APIInteraction } from 'discord-api-types/v10'
-import { sleep } from 'utils'
 import { fetcher } from 'utils/browser'
 
 import { commandMap } from '$lib/commands'
-import { verify } from '$lib/discord'
+import { editInteractionResponse, verify } from '$lib/discord'
 
 import type { RequestHandler } from './$types'
 
@@ -30,31 +29,24 @@ export const POST: RequestHandler = async ({ request, fetch: fetch_ }) => {
 
       const fetch = fetcher(fetch_)
 
-      const response = await Promise.race([
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
-        command.handler(message as any, { fetch }),
-        sleep(2500).then(async () => {
-          await fetch('/api/interactions/node', {
-            method: 'POST',
-            headers: request.headers,
-            body: rawBody,
-          })
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
+      const res = await command.handler(message as any, { fetch })
 
-          return {
-            type: InteractionResponseType.ChannelMessageWithSource,
-            data: {
-              content: 'Loading...',
-            },
-          }
-        }),
-      ])
+      if (res.type === InteractionResponseType.ChannelMessageWithSource) {
+        console.log('Editing response...')
+        await editInteractionResponse(fetch)(message.token, res.data)
+      } else {
+        console.log('Not editing response')
+      }
 
-      console.log('Responding with', response)
-
-      return json(response)
+      return json(res)
     }
     default: {
       throw error(400, 'Bad request type')
     }
   }
+}
+
+export const config = {
+  runtime: 'nodejs18.x',
 }
