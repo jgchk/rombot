079f3fc4a00ef5c6abd5bf19cf63fc17c7bdcc92
Jake Cheek <jake@f-m.fm>
2023-05-06 19:48:46 -0400

    put response editing back in edge func

--- apps/app/src/routes/api/interactions/+server.ts
+++ apps/app/src/routes/api/interactions/+server.ts
@@ -6,7 +6,7 @@ import { sleep } from 'utils'
 import { fetcher } from 'utils/browser'
 
 import { commandMap } from '$lib/commands'
-import { verify } from '$lib/discord'
+import { editInteractionResponse, verify } from '$lib/discord'
 import { env } from '$lib/env'
 
 import type { RequestHandler } from './$types'
@@ -36,47 +36,27 @@ export const POST: RequestHandler = async ({ request, fetch: fetch_ }) => {
 
       const fetch = fetcher(fetch_)
 
-      if (command.runtime === 'node') {
-        console.log('Running with Node...')
-
-        await fetch('/api/interactions/node', {
-          method: 'POST',
-          headers: request.headers,
-          body: rawBody,
-        })
-
-        return json({
-          type: InteractionResponseType.ChannelMessageWithSource,
-          data: {
-            content: 'Loading...',
-          },
-        })
-      } else {
-        console.log('Running with Edge...')
-      }
-
       const db = (await getDatabase)({ connectionString: env.DATABASE_URL })
 
       let responded = false
       const response = await Promise.race([
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
-        command.handler(message as any, { fetch, db }),
-        sleep(2500).then(() => {
-          if (!responded) {
-            void fetch('/api/interactions/node', {
-              method: 'POST',
-              headers: request.headers,
-              body: rawBody,
-            })
-          }
-
-          return {
-            type: InteractionResponseType.ChannelMessageWithSource,
-            data: {
-              content: 'Loading...',
-            },
+        Promise.resolve(command.handler(message as any, { fetch, db })).then(async (res) => {
+          if (responded) {
+            if (res.type === InteractionResponseType.ChannelMessageWithSource) {
+              console.log('Editing response...')
+              await editInteractionResponse(fetch)(message.token, res.data)
+            } else {
+              console.log('Not editing response')
+            }
           }
         }),
+        sleep(2500).then(() => ({
+          type: InteractionResponseType.ChannelMessageWithSource,
+          data: {
+            content: 'Loading...',
+          },
+        })),
       ])
       responded = true
 
