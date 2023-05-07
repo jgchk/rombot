9b6e55d50ec0537e95a92e83d191aa6c15ff5854
Jake Cheek <jake@f-m.fm>
2023-05-06 21:58:04 -0400

    clean up error embed response

--- apps/app/src/lib/commands/utils.ts
+++ apps/app/src/lib/commands/utils.ts
@@ -14,6 +14,7 @@ import type {
   APIEmbed,
   ApplicationCommandOptionType,
 } from 'discord'
+import { toErrorString } from 'utils'
 
 export type OptionType<T extends ApplicationCommandOptionType> =
   T extends ApplicationCommandOptionType.Subcommand
@@ -50,56 +51,9 @@ export const isOption =
   (option: APIApplicationCommandInteractionDataOption): option is OptionType<T> =>
     option.name === name && option.type === type
 
-export const getErrorEmbed = (error: string): APIEmbed => ({
+export const getErrorEmbed = (error: unknown): APIEmbed => ({
   color: 3447003,
-  author: {
-    name: 'Author Name, it can hold 256 characters',
-    icon_url: 'https://i.imgur.com/lm8s41J.png',
-  },
-  thumbnail: {
-    url: 'http://i.imgur.com/p2qNFag.png',
-  },
-  image: {
-    url: 'http://i.imgur.com/yVpymuV.png',
-  },
-  title: 'This is your title, it can hold 256 characters',
-  url: 'https://discord.js.org/#/docs/main/master/class/MessageEmbed',
-  description: 'This is the main body of text, it can hold 2048 characters.',
-  fields: [
-    {
-      name: 'This is a single field title, it can hold 256 characters',
-      value: 'This is a field value, it can hold 1024 characters.',
-      inline: false,
-    },
-    {
-      name: 'Error',
-      value: error,
-      inline: false,
-    },
-    {
-      name: 'Inline fields',
-      value: 'They can have different fields with small headlines, and you can inline them.',
-      inline: true,
-    },
-    {
-      name: 'Masked links',
-      value:
-        'You can put [masked links](https://discord.js.org/#/docs/main/master/class/MessageEmbed) inside of rich embeds.',
-      inline: true,
-    },
-    {
-      name: 'Markdown',
-      value: 'You can put all the *usual* **__Markdown__** inside of them.',
-      inline: true,
-    },
-    {
-      name: '\u200b',
-      value: '\u200b',
-    },
-  ],
+  title: 'Error',
+  description: toErrorString(error),
   timestamp: new Date().toString(),
-  footer: {
-    icon_url: 'http://i.imgur.com/w1vhFSR.png',
-    text: 'This is the footer text, it can hold 2048 characters',
-  },
 })
