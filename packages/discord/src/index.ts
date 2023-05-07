c3900ca098cf5481e3366ee37de9f94f4c7bf189
Jake Cheek <jake@f-m.fm>
2023-05-07 18:33:12 -0400

    set content type of payload_json to application/json

--- packages/discord/src/index.ts
+++ packages/discord/src/index.ts
@@ -39,7 +39,12 @@ export const Discord = (
       files: File[]
     ) => {
       const formData = new FormData()
-      formData.append('payload_json', JSON.stringify(response))
+      formData.append(
+        'payload_json',
+        new Blob([JSON.stringify(response)], {
+          type: 'application/json',
+        })
+      )
 
       for (const [i, file] of files.entries()) {
         formData.append(`files[${i}]`, file)
